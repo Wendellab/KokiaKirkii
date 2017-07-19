72a69ea88e633f6b083a9d74ff5c83f8a60d55d5
Justin Conover <jconover@iastate.edu>
2017-07-19 12:35:14 -0500

    updating manuscript, moving figure to correct location

--- analysis/Ks_CNV/DnDs.R
+++ analysis/Ks_CNV/DnDs.R
@@ -8,6 +8,7 @@ library("reshape2")
 ######
 
 DF <- read.csv("final_dNdS.csv", header = TRUE)
+Malvaceae_DF <- read.csv("Cotton_Chocolate_final_dNdS.csv", header = TRUE)
 
 
 ######
@@ -21,7 +22,7 @@ DF$`Species Comparison` <- as.factor(rep(c("G. raimondii vs. G. kirkii", "G. rai
 filtered <- !(DF$X %in% DF[DF$dS > 0.6,]$X)
 DF <- DF[filtered,]
 
-
+Malvaceae_DF <- Malvaceae_DF[Malvaceae_DF$dS<3,]
 
 
 ######
@@ -32,33 +33,32 @@ groups <- DF %>% group_by(`Species Comparison`)
 dS.summary.table <- summarize(groups, Mean = mean(dS), StDev = sd(dS), Median = median(dS))
 dN.summary.table <- summarize(groups, Mean = mean(dN), StDev = sd(dN), Median = median(dN))
 
-malv.time <- 0.00000000456
-palms.time <- 0.0000000026
-brassica.time <- 0.000000015
+malv.time.median <- median(Malvaceae_DF$dS) / (2*60000000)
 
-Gossypioides.Kokia.time = c(dS.summary.table[[1,4]] / (2*brassica.time) /1000000, dS.summary.table[[1,4]] / (2*malv.time) /1000000, dS.summary.table[[1,4]] / (2*palms.time) /1000000)
-Gossypium.Gossypioides.time = c(dS.summary.table[[2,4]] / (2*brassica.time) /1000000, dS.summary.table[[2,4]] / (2*malv.time) /1000000, dS.summary.table[[2,4]] / (2*palms.time) /1000000)
-Gossypium.Kokia.time = c(dS.summary.table[[3,4]] / (2*brassica.time) /1000000, dS.summary.table[[3,4]] / (2*malv.time) /1000000, dS.summary.table[[3,4]] / (2*palms.time) /1000000)
+Gossypioides.Kokia.time = c(dS.summary.table[[1,4]] / (2*malv.time.median) /1000000)
+Gossypium.Gossypioides.time = c(dS.summary.table[[2,4]] / (2*malv.time.median) /1000000)
+Gossypium.Kokia.time = c(dS.summary.table[[3,4]] / (2*malv.time.median) /1000000)
 Divergence.table <- rbind(Gossypioides.Kokia.time, Gossypium.Gossypioides.time, Gossypium.Kokia.time)
-colnames(Divergence.table) <- c("Brassica Estimate", "Malvaceae Estimate", "Palms Estimate")
+colnames(Divergence.table) <- c("Malvaceae Estimate")
 
+
+Df.melted <- melt(DF, id.vars = "Species Comparison", measure.vars = c("dS", "dN"))
+colnames(Df.melted)[3] <- c("Synonymous Substitution Rate")
+
+newDf.melted <- Df.melted[Df.melted$variable == "dS",]
 
 ######
 #Plotting
 ######
+
 png("Figure_dNdSboxplot.png", 5000, 5000, pointsize=12, res=600)
-Df.melted <- melt(DF, id.vars = "Species Comparison", measure.vars = c("dS", "dN"))
-colnames(Df.melted)[3] <- c("Synonymous Substitution Rate")
-
-
-p <- ggplot(Df.melted, aes(x=`Species Comparison`, y = `Synonymous Substitution Rate`, fill=variable)) + geom_boxplot(position = position_dodge(1), outlier.shape = NA) + 
+ggplot(Df.melted, aes(x=`Species Comparison`, y = `Synonymous Substitution Rate`, fill=variable)) + geom_boxplot(position = position_dodge(1), outlier.shape = NA) + 
   theme(legend.position = c(0.95,0.9), legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_text(color = "black", size = 13)) + 
-  scale_y_continuous(limits = c(0,0.175))
-p + stat_summary(fun.y=median, geom="point", size=1, color="white", position = position_dodge(1)) + scale_fill_manual(values = c("red3", "black"))
-
+  scale_y_continuous(limits = c(0,0.175)) +
+  stat_summary(fun.y=median, geom="point", size=1, color="white", position = position_dodge(1)) + scale_fill_manual(values = c("red3", "black"))
 dev.off()
 
-newDf.melted <- Df.melted[Df.melted$variable == "dS",]
+
 
 
 png("Figure_dSDistribution.png", 5000, 5000, pointsize=12, res=600)
@@ -66,8 +66,15 @@ ggplot(newDf.melted, aes(newDf.melted$`Synonymous Substitution Rate`, colour = n
   geom_density(adjust = 1/5) + 
   theme(legend.position = c(0.8, 0.9), axis.title.y = element_blank(), legend.title = element_blank()) + 
   xlab("Synonymous Substitution Rate") 
+dev.off()
 
 
 
+
+png("Figure_Malvaceae_Divergence.png", 5000, 5000, pointsize=12, res=600)
+ggplot(Malvaceae_DF, aes(Malvaceae_DF$dS)) + 
+  geom_density(adjust = 1/5) + geom_vline(xintercept = median(Malvaceae_DF$dS)) +
+  theme(axis.title.y = element_blank(), legend.title = element_blank()) + 
+  xlab("Synonymous Substitution Rate") 
 dev.off()
 
